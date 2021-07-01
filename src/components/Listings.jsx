97deb8610986dee4fc319f75bb90e9f38945f7c7
blaqbox-prime <karabosambo@outlook.com>
2021-07-01 12:06:45 +0200

    m

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col} from 'react-bootstrap'
 import Stay from './Stay'
 import './Listing.css'
 import {useFilter} from '../contexts/FilterContext'
